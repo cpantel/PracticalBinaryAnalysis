9a39aad31d0c5c1deb5d7a359d5e870ad87c59a8
Carlos Pantelides <cpantel@github.com>
2019-11-19 06:34:36 -0300

    different values

--- introspect/explicit_no_zero.c
+++ introspect/explicit_no_zero.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-#define BUFFER_LENGTH 32
 
-int globalVar = 42;
+int globalVar = 41;
 static int staticGlobalVar = 42;
 
 int main(int argc, char* argv[]) {
-   int localVar = 42;
-   static int staticLocalVar = 42;
+   int localVar = 43;
+   static int staticLocalVar = 44;
 
    printf("globalVar       address %p\n" , &globalVar);
    printf("staticGlobalVar address %p\n" , &staticGlobalVar);
